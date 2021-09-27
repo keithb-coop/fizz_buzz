7d56688fd670bcc6783075b670d47e372a275887
Keith Braithwaite <keith.braithwaite@coop.co.uk>
2021-09-27 17:29:14 +0100

    hang on, this isn't proper fizzbuzz anyway! doesn't cover the mutliple cases. Still, now no if!

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,10 +1,12 @@
 def fizz_buzz(number):
-    flags = {k : number % k == 0 for k in [3,5,7]}
-    if flags[3]:
-        return 'Fizz'
-    if flags[5]:
-        return 'Buzz'
-    if flags[7]:
-        return 'Baz'
+    divisors = filter(lambda k: number % k == 0, [1,3,5,7])
+    largest_divisor = max(divisors)
+
+    printers = {1: (lambda n: n),
+                3: (lambda n: "Fizz"),
+                5: (lambda n: "Buzz"),
+                7: (lambda n: "Baz")}
+
+    return printers[largest_divisor](number)
 
     return number
