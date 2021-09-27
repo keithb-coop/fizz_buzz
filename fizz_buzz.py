cf5685fb76faad1faf1176b4b9c88e72fec23cd9
Keith Braithwaite <keith.braithwaite@coop.co.uk>
2021-09-27 17:29:42 +0100

    No need for this intermediate

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,12 +1,10 @@
 def fizz_buzz(number):
     divisors = filter(lambda k: number % k == 0, [1,3,5,7])
-    largest_divisor = max(divisors)
-
     printers = {1: (lambda n: n),
                 3: (lambda n: "Fizz"),
                 5: (lambda n: "Buzz"),
                 7: (lambda n: "Baz")}
 
-    return printers[largest_divisor](number)
+    return printers[max(divisors)](number)
 
     return number
