2b4af1804879cd2b1527f4202496dded974303d9
Keith Braithwaite <keith.braithwaite@coop.co.uk>
2021-09-27 17:02:07 +0100

    reverse expexted and actual so that failure messages read more naturally

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -6,31 +6,31 @@ from fizz_buzz import fizz_buzz
 class TestFizzBuzz(unittest.TestCase):
     def test_one_returns_one(self):
         result = fizz_buzz(1)
-        self.assertEqual(result, 1)
+        self.assertEqual(1, result)
 
     def test_three_returns_fizz(self):
         result = fizz_buzz(3)
-        self.assertEqual(result, 'Fizz')
+        self.assertEqual('Fizz', result)
 
     def test_five_returns_buzz(self):
         result = fizz_buzz(5)
-        self.assertEqual(result, 'Buzz')
+        self.assertEqual('Buzz', result)
     
     def test_seven_returns_baz(self):
         result = fizz_buzz(7)
-        self.assertEqual(result, 'Baz')
+        self.assertEqual('Baz', result)
 
     def test_multiple_of_three_returns_fizz(self):
         result = fizz_buzz(6)
-        self.assertEqual(result, 'Fizz')
+        self.assertEqual('Fizz', result)
 
     def test_multiple_of_five_returns_buzz(self):
         result = fizz_buzz(10)
-        self.assertEqual(result, 'Buzz')
+        self.assertEqual('Buzz', result)
     
     def test_multiple_of_seven_returns_baz(self):
         result = fizz_buzz(14)
-        self.assertEqual(result, 'Baz')
+        self.assertEqual('Baz', result)
 
 
 if __name__ == "__main__":
