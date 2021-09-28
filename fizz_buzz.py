e39ddaa7130c96cc21f1104b0b5cd2685dbaec30
Keith Braithwaite <keith.braithwaite@coop.co.uk>
2021-09-28 11:52:49 +0100

    Turns out that's contr PEP203 and PEP201

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -3,24 +3,26 @@ from collections import defaultdict
 
 def fizz_buzz(number):
     justANumber = JustANumber()
-    baz  = StringANumber(7, "Baz", justANumber)
-    bar  = StringANumber(5, "Buzz", baz)
+    baz = StringANumber(7, "Baz", justANumber)
+    bar = StringANumber(5, "Buzz", baz)
     fizz = StringANumber(3, "Fizz", bar)
 
-    return fizz.stringThis(number,"")
+    return fizz.stringThis(number, "")
+
 
 class StringANumber:
-    def __init__(self, mulitpleOf = None, stringToUse = None, next = None):
+    def __init__(self, mulitpleOf=None, stringToUse=None, next=None):
         self.multipleOf = mulitpleOf
         self.stringToUse = stringToUse
         self.next = next
 
     def stringThis(self, number, stringSoFar):
-        handlers = {1               : (lambda n: self.next.stringThis(n, stringSoFar)),
-                    self.multipleOf : (lambda n: self.stringToUse)}
+        handlers = {1: (lambda n: self.next.stringThis(n, stringSoFar)),
+                    self.multipleOf: (lambda n: self.stringToUse)}
 
         divisors = filter(lambda k: number % k == 0, [1, self.multipleOf])
         return handlers[max(divisors)](number)
+
 
 class JustANumber(StringANumber):
     def __init__(self):
